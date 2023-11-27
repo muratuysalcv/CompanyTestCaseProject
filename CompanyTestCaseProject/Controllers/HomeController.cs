06ccf4a06a08e2576670943e605f1f42348e5742
Murat Uysal <muratuysalcv@gmail.com>
2023-11-27 12:50:02 +0300

    first load.

---
+++ CompanyTestCaseProject/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using CompanyTestCaseProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace CompanyTestCaseProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //main list of student list.
+            var ogrenciListesi = new List<Ogrenci>();
+            var rootFolder = Directory.GetCurrentDirectory();
+            // read file contents by line.
+            var lines = System.IO.File.ReadAllLines(rootFolder + "//ogrenciler.txt");
+            for (var i = 0; i < lines.Length; i += 1)
+            {
+                var line = lines[i];
+                var ad = line.Split(" ")[0];
+                int no = 0;
+                int vize = 0;
+                int final = 0;
+                try
+                {
+                    // convert text to numbers and throw exception if not numeric.
+                    no = Convert.ToInt32(line.Split(" ")[1]);
+                    vize = Convert.ToInt32(line.Split(" ")[2]);
+                    final = Convert.ToInt32(line.Split(" ")[3]);
+                }
+                catch (Exception ex)
+                {
+                    // if not numeric value
+                    ad += " ERROR : ogrenci no, vize veya final notu alanlarının sayısal değer olmalıdır.";
+                }
+                // set values to object and added to list.
+                ogrenciListesi.Add(new Ogrenci(no, ad, vize, final));
+            }
+
+            // all calculated result text in a list.
+            var notlar = ogrenciListesi.Select(x => x.OgrenciYazdir());
+
+            // join with \n for line by viewing.
+            var sonucMetni = string.Join("\n", notlar);
+
+            // return result text.
+            return Content(sonucMetni);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CompanyTestCaseProject/Ogrenci.cs
@@ -0,0 +1,27 @@
+﻿namespace CompanyTestCaseProject
+{
+    public class Ogrenci
+    {
+        public Ogrenci(int no, string ad, int vizeNotu, int finalNotu)
+        {
+            this.No = no;
+            this.Ad = ad;
+            this.FinalNotu = finalNotu;
+            this.VizeNotu = vizeNotu;
+        }
+        private int No { get; set; }
+        private string Ad { get; set; }
+        private int VizeNotu { get; set; }
+        private int FinalNotu { get; set; }
+
+        private double NotHesapla()
+        {
+            return (this.VizeNotu * 0.4 + this.FinalNotu * 0.6);
+        }
+
+        public string OgrenciYazdir()
+        {
+            return "Ogrenci bilgileri:" + this.Ad + " adında " + this.No + " numarasinda not ortalamasi da "+this.NotHesapla().ToString("n1") ;
+        }
+    }
+}
